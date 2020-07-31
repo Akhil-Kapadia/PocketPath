3ce31d80780d4954b1d5e2f2105ce300d66941b3
Akhil Kapadia <akhil.kapadia@ttu.edu>
2020-07-31 17:07:26 -0500

    Begin development of transmit state

--- pocketpath.ino
+++ pocketpath.ino
@@ -33,7 +33,7 @@ String name;
 
 //Objs
 File sensorData;
-WiFiClient wifi;
+WiFiServer server(80);
 
 //function declartions
 void interruptGPS();
@@ -42,6 +42,9 @@ void interruptTRANSMIT();
 void sleepRoutine();
 String aquireGPSdata();
 void saveData(String data);
+void wifiSetup();
+void printWifiStatus();
+void webpage();
 
 void setup()
 {
@@ -65,15 +68,17 @@ void setup()
   }
 
   //create new CSV file numerical named per session.
-    //create new file
-  for (int n = 0; n < LIMIT; n++) {
+  //create new file
+  for (int n = 0; n < LIMIT; n++)
+  {
     sprintf(fileName, "DLOG%.3d.CSV", n);
-    if (SD.exists(fileName)) continue;
+    if (SD.exists(fileName))
+      continue;
     sensorData = SD.open(fileName, FILE_WRITE);
-    Serial.println("File: "+ String(fileName));
-    break;      
+    Serial.println("File: " + String(fileName));
+    break;
   }
-   sensorData.close();
+  sensorData.close();
 
   //Interrupt subroutines.
   LowPower.attachInterruptWakeup(RTC_ALARM_WAKEUP, interruptGPS, CHANGE);
@@ -84,16 +89,15 @@ void setup()
   pinMode(LED_TRACKING, OUTPUT);
   pinMode(LED_SLEEPING, OUTPUT);
   pinMode(LED_TRANSMITTING, OUTPUT);
-  
-}
 
-void loop()
-{
-  //Change states here according to buttons or whatever.
-
-  switch (state)
+  void loop()
   {
-  case SLEEPING:
+    //Change states here according to buttons or whatever.
+
+    switch (state)
+    {
+    case SLEEPING:
+    }
     Serial.println("In Sleep mode...");
     //Set LEDS.
     analogWrite(LED_SLEEPING, 128); //Use PWM to save current consumption.
@@ -143,12 +147,14 @@ void interruptGPS()
 }
 
 //Interrupt service routine if paused button is pressed.
-void interruptPAUSED() {
+void interruptPAUSED()
+{
   paused = true;
 }
 
 //Interrupts service routing is transmit button is pressed. Changes state.
-void interruptTRANSMIT() {
+void interruptTRANSMIT()
+{
   state = TRANSMITTING;
 }
 
@@ -211,8 +217,111 @@ void saveData(String data)
 
 /**  TRANSMITTING STATE CODE SECTION
  * 
- * DEPENDENCIES : ArduionIoTCloud, SDFAT, SPI.
+ * DEPENDENCIES : WiFiNINA, SD, SPI.
  * 
- * 
- * 
+ * This section of code handles the wireless communication of data over wifi. 
+ * A simple web server will display the GPS data information from CSV files stored in the SD card, 
+ * and can display past sessions by interactin with the webpage created by the arduino.
  */
+
+//Initizes connection to wifi network.
+void wifiSetup()
+{
+  // attempt to connect to Wifi network:
+  while (status != WL_CONNECTED)
+  {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(SECRET_SSID, SECRET_PASS);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+  server.begin();
+  //Connected now, so print out the status.
+  printWifiStatus();
+}
+
+//prints wifi connection status.
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+
+
+//Begin displaying webpage to client connected to webserver.
+void webpage()
+{
+  // listen for incoming clients
+  WiFiClient client = server.available();
+  if (client)
+  {
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected())
+    {
+      if (client.available())
+      {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank)
+        {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close"); // the connection will be closed after completion of the response
+          client.println("Refresh: 5");        // refresh the page automatically every 5 sec
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+          // output the value of each analog input pin
+          for (int analogChannel = 0; analogChannel < 6; analogChannel++)
+          {
+            int sensorReading = analogRead(analogChannel);
+            client.print("analog input ");
+            client.print(analogChannel);
+            client.print(" is ");
+            client.print(sensorReading);
+            client.println("<br />");
+          }
+          client.println("</html>");
+          break;
+        }
+        if (c == '\n')
+        {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        }
+        else if (c != '\r')
+        {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+
+    // close the connection:
+    client.stop();
+    Serial.println("client disonnected");
+  }
+}
+}
